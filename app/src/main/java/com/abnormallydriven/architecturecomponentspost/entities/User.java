873f97859d7f41ff66fa36f9c41a0d7220ad5bc7
Brett <bretttucker@gmail.com>
2017-05-29 12:08:40 -0600

    Add application entities

---
+++ app/src/main/java/com/abnormallydriven/architecturecomponentspost/entities/User.java
@@ -0,0 +1,60 @@
+package com.abnormallydriven.architecturecomponentspost.entities;
+
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int age;
+
+    private String gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
