954889d445a3608fde5487bef75baa9d7deee7b4
Brett <bretttucker@gmail.com>
2017-06-28 21:44:52 -0600

    Add UserMeasurementActivity
    Adds the activity that will be used to display a user's measurements.

--- app/src/main/java/com/abnormallydriven/architecturecomponentspost/common/NavigationController.java
+++ app/src/main/java/com/abnormallydriven/architecturecomponentspost/common/NavigationController.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import com.abnormallydriven.architecturecomponentspost.addmeasurements.AddMeasurementsActivity;
 import com.abnormallydriven.architecturecomponentspost.adduser.AddUserActivity;
 import com.abnormallydriven.architecturecomponentspost.common.data.entities.User;
+import com.abnormallydriven.architecturecomponentspost.usermeasurements.UserMeasurementsActivity;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -24,6 +25,12 @@ public class NavigationController {
     public void navigateToUserAdd(){
         //TODO activity intent factories
         appContext.startActivity(new Intent(appContext, AddUserActivity.class));
+    }
+
+    public void navigateToUserMeasurements(User selectedUser){
+        Intent intent = new Intent(appContext, UserMeasurementsActivity.class);
+        intent.putExtra("user", selectedUser);
+        appContext.startActivity(intent);
     }
 
     public void navigateToAddMeasurement(User selectedUser){
---
+++ app/src/main/java/com/abnormallydriven/architecturecomponentspost/usermeasurements/UserMeasurementsActivity.java
@@ -0,0 +1,15 @@
+package com.abnormallydriven.architecturecomponentspost.usermeasurements;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.abnormallydriven.architecturecomponentspost.R;
+
+public class UserMeasurementsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_measurements);
+    }
+}
