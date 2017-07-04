b18470c73402689eb28e6141f994029cdf195d48
Brett <bretttucker@gmail.com>
2017-07-04 10:29:45 -0600

    Add a BindingConverter for dates

---
+++ app/src/main/java/com/abnormallydriven/architecturecomponentspost/common/BindingConverters.java
@@ -0,0 +1,18 @@
+package com.abnormallydriven.architecturecomponentspost.common;
+
+import android.databinding.BindingConversion;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class BindingConverters {
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
+
+    @BindingConversion
+    public static String convertDate(Date date){
+        return SIMPLE_DATE_FORMAT.format(date);
+    }
+
+}
