d9eb01cc24355481b261dd32e2bd07fe86b75934
Brett <bretttucker@gmail.com>
2017-07-08 15:17:17 -0600

    Refactor away from two way databinding
    The add measurement activity will no longer two way databind. It was too
    error prone and felt a little awkward.

--- app/src/main/java/com/abnormallydriven/architecturecomponentspost/addmeasurements/AddMeasurementsActivity.java
+++ app/src/main/java/com/abnormallydriven/architecturecomponentspost/addmeasurements/AddMeasurementsActivity.java
@@ -1,24 +1,27 @@
 package com.abnormallydriven.architecturecomponentspost.addmeasurements;
 
-import android.arch.lifecycle.Lifecycle;
-import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.databinding.Observable;
-import android.databinding.ViewDataBinding;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.abnormallydriven.architecturecomponentspost.R;
 import com.abnormallydriven.architecturecomponentspost.common.ApplicationViewModelFactory;
 import com.abnormallydriven.architecturecomponentspost.common.data.entities.Measurement;
+import com.abnormallydriven.architecturecomponentspost.common.data.entities.User;
 import com.abnormallydriven.architecturecomponentspost.databinding.ActivityAddMeasurementsBinding;
 
-import javax.annotation.Nullable;
+import java.util.Date;
+
 import javax.inject.Inject;
 
 import dagger.android.AndroidInjection;
@@ -31,7 +34,6 @@ public class AddMeasurementsActivity extends AppCompatActivity implements Lifecy
     @Inject
     ApplicationViewModelFactory applicationViewModelFactory;
 
-    @Nullable
     AddMeasurementsViewModel viewModel;
 
     private ActivityAddMeasurementsBinding binding;
@@ -62,6 +64,27 @@ public class AddMeasurementsActivity extends AppCompatActivity implements Lifecy
         }
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_add_measurements_menu, menu);
+        Drawable drawable = menu.findItem(R.id.save_measurement_item).getIcon();
+
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableCompat.setTint(drawable, ContextCompat.getColor(this, android.R.color.white));
+        menu.findItem(R.id.save_measurement_item).setIcon(drawable);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.save_measurement_item){
+            viewModel.onSaveMeasurementForUserClick(getMeasurementFromFields());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     private void showMeasurementSavedMessage() {
         viewModel.shouldShowSaveSuccessMessage.set(false);
@@ -70,6 +93,48 @@ public class AddMeasurementsActivity extends AppCompatActivity implements Lifecy
 
     private void setupViewModel() {
         viewModel = ViewModelProviders.of(this, applicationViewModelFactory).get(AddMeasurementsViewModel.class);
+    }
+
+    private Measurement getMeasurementFromFields(){
+        final Measurement currentMeasurement = new Measurement();
+
+        currentMeasurement.setLowerLeftBicep(safelyConvertFloat(binding.lowerLeftBicepContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLowerRightBicep(safelyConvertFloat(binding.lowerRightBicepContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLeftBicep(safelyConvertFloat(binding.leftBicepContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setRightBicep(safelyConvertFloat(binding.rightBicepContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLeftCalf(safelyConvertFloat(binding.leftCalfContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setRightCalf(safelyConvertFloat(binding.rightCalfContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setChest(safelyConvertFloat(binding.chestContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLeftForearm(safelyConvertFloat(binding.leftForearmContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setRightForearm(safelyConvertFloat(binding.rightForearmContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setHighHip(safelyConvertFloat(binding.highHipContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setHip(safelyConvertFloat(binding.hipContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLowerLeftThigh(safelyConvertFloat(binding.lowerLeftThighContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLowerRightThigh(safelyConvertFloat(binding.lowerRightThighContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setMidLeftThigh(safelyConvertFloat(binding.midLeftThighContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setMidRightThigh(safelyConvertFloat(binding.midRightThighContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setUpperLeftThigh(safelyConvertFloat(binding.upperLeftThighContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setUpperRightThigh(safelyConvertFloat(binding.upperRightThighContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setAbdominalWaist(safelyConvertFloat(binding.abdominalWaistContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setLowerWaist(safelyConvertFloat(binding.lowerWaistContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setNarrowestWaist(safelyConvertFloat(binding.narrowestWaistContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setWeight(safelyConvertFloat(binding.weightContainer.measurementValueEditText.getText().toString()));
+        currentMeasurement.setMeasurementDate(new Date());
+        currentMeasurement.setUserId(getUserFromIntent().getId());
+
+        return currentMeasurement;
+    }
+
+    private float safelyConvertFloat(String floatString){
+        try{
+            return Float.valueOf(floatString);
+        } catch(NumberFormatException ex){
+            return 0.0f;
+        }
+    }
+
+    private User getUserFromIntent(){
+        return getIntent().getParcelableExtra("user");
     }
 
     @Override
--- app/src/main/java/com/abnormallydriven/architecturecomponentspost/addmeasurements/AddMeasurementsViewModel.java
+++ app/src/main/java/com/abnormallydriven/architecturecomponentspost/addmeasurements/AddMeasurementsViewModel.java
@@ -32,69 +32,8 @@ public class AddMeasurementsViewModel extends ViewModel {
     private final MeasurementDao measurementDao;
 
     @NonNull
-    private final ObservableFloat lowerLeftBicep;
-
-    @NonNull
-    private final ObservableFloat lowerRightBicep;
-
-    @NonNull
-    private final ObservableFloat leftBicep;
-
-    @NonNull
-    private final ObservableFloat rightBicep;
-
-    @NonNull
-    private final ObservableFloat leftCalf;
-
-    @NonNull
-    private final ObservableFloat rightCalf;
-
-    @NonNull
-    private final ObservableFloat chest;
-
-    @NonNull
-    private final ObservableFloat leftForearm;
-
-    @NonNull
-    private final ObservableFloat rightForearm;
-
-    @NonNull
-    private final ObservableFloat highHip;
-
-    @NonNull
-    private final ObservableFloat hip;
-
-    @NonNull
-    private final ObservableFloat lowerLeftThigh;
-
-    @NonNull
-    private final ObservableFloat lowerRightThigh;
-
-    @NonNull
-    private final ObservableFloat midLeftThigh;
-
-    @NonNull
-    private final ObservableFloat midRightThigh;
-
-    @NonNull
-    private final ObservableFloat upperLeftThigh;
-
-    @NonNull
-    private final ObservableFloat upperRightThigh;
-
-    @NonNull
-    private final ObservableFloat abdominalWaist;
-
-    @NonNull
-    private final ObservableFloat lowerWaist;
-
-    @NonNull
-    private final ObservableFloat narrowestWaist;
-
-    @NonNull
     public final ObservableBoolean shouldShowProgressSpinner;
 
-    @NonNull
     public final ObservableBoolean shouldShowSaveSuccessMessage;
 
     @Inject
@@ -104,195 +43,25 @@ public class AddMeasurementsViewModel extends ViewModel {
         this.uiExecutor = uiExecutor;
         this.diskExecutor = diskExecutor;
         this.measurementDao = measurementDao;
-        this.shouldShowProgressSpinner = new ObservableBoolean(false);
-        this.shouldShowSaveSuccessMessage = new ObservableBoolean(false);
 
-        lowerLeftBicep = new ObservableFloat(0.0f);
-        lowerRightBicep = new ObservableFloat(0.0f);
-        leftBicep = new ObservableFloat(0.0f);
-        rightBicep = new ObservableFloat(0.0f);
-        leftCalf = new ObservableFloat(0.0f);
-        rightCalf = new ObservableFloat(0.0f);
-        chest = new ObservableFloat(0.0f);
-        leftForearm = new ObservableFloat(0.0f);
-        rightForearm = new ObservableFloat(0.0f);
-        highHip = new ObservableFloat(0.0f);
-        hip = new ObservableFloat(0.0f);
-        lowerLeftThigh = new ObservableFloat(0.0f);
-        lowerRightThigh = new ObservableFloat(0.0f);
-        midLeftThigh = new ObservableFloat(0.0f);
-        midRightThigh = new ObservableFloat(0.0f);
-        upperLeftThigh = new ObservableFloat(0.0f);
-        upperRightThigh = new ObservableFloat(0.0f);
-        abdominalWaist = new ObservableFloat(0.0f);
-        lowerWaist = new ObservableFloat(0.0f);
-        narrowestWaist = new ObservableFloat(0.0f);
+        shouldShowProgressSpinner = new ObservableBoolean(false);
+        shouldShowSaveSuccessMessage = new ObservableBoolean(false);
     }
 
-
-    @NonNull
-    public ObservableFloat getLowerLeftBicep() {
-        return lowerLeftBicep;
-    }
-
-    @NonNull
-    public ObservableFloat getLowerRightBicep() {
-        return lowerRightBicep;
-    }
-
-    @NonNull
-    public ObservableFloat getLeftBicep() {
-        return leftBicep;
-    }
-
-    @NonNull
-    public ObservableFloat getRightBicep() {
-        return rightBicep;
-    }
-
-    @NonNull
-    public ObservableFloat getLeftCalf() {
-        return leftCalf;
-    }
-
-    @NonNull
-    public ObservableFloat getRightCalf() {
-        return rightCalf;
-    }
-
-    @NonNull
-    public ObservableFloat getChest() {
-        return chest;
-    }
-
-    @NonNull
-    public ObservableFloat getLeftForearm() {
-        return leftForearm;
-    }
-
-    @NonNull
-    public ObservableFloat getRightForearm() {
-        return rightForearm;
-    }
-
-    @NonNull
-    public ObservableFloat getHighHip() {
-        return highHip;
-    }
-
-    @NonNull
-    public ObservableFloat getHip() {
-        return hip;
-    }
-
-    @NonNull
-    public ObservableFloat getLowerLeftThigh() {
-        return lowerLeftThigh;
-    }
-
-    @NonNull
-    public ObservableFloat getLowerRightThigh() {
-        return lowerRightThigh;
-    }
-
-    @NonNull
-    public ObservableFloat getMidLeftThigh() {
-        return midLeftThigh;
-    }
-
-    @NonNull
-    public ObservableFloat getMidRightThigh() {
-        return midRightThigh;
-    }
-
-    @NonNull
-    public ObservableFloat getUpperLeftThigh() {
-        return upperLeftThigh;
-    }
-
-    @NonNull
-    public ObservableFloat getUpperRightThigh() {
-        return upperRightThigh;
-    }
-
-    @NonNull
-    public ObservableFloat getAbdominalWaist() {
-        return abdominalWaist;
-    }
-
-    @NonNull
-    public ObservableFloat getLowerWaist() {
-        return lowerWaist;
-    }
-
-    @NonNull
-    public ObservableFloat getNarrowestWaist() {
-        return narrowestWaist;
-    }
-
-    void onSaveMeasurementForUserClick(final User user) {
+    void onSaveMeasurementForUserClick(final Measurement measurement) {
         shouldShowProgressSpinner.set(true);
 
         diskExecutor.execute(new Runnable() {
             @Override
             public void run() {
-                Measurement newMeasurement = new Measurement();
-
-                newMeasurement.setLowerLeftBicep(lowerLeftBicep.get());
-                newMeasurement.setLowerRightBicep(lowerRightBicep.get());
-                newMeasurement.setLeftBicep(leftBicep.get());
-                newMeasurement.setRightBicep(rightBicep.get());
-                newMeasurement.setLeftCalf(leftCalf.get());
-                newMeasurement.setRightCalf(rightCalf.get());
-                newMeasurement.setChest(chest.get());
-                newMeasurement.setLeftForearm(leftForearm.get());
-                newMeasurement.setRightForearm(rightForearm.get());
-                newMeasurement.setHighHip(highHip.get());
-                newMeasurement.setHighHip(hip.get());
-                newMeasurement.setLowerLeftThigh(lowerLeftThigh.get());
-                newMeasurement.setLowerRightThigh(lowerRightThigh.get());
-                newMeasurement.setMidLeftThigh(midLeftThigh.get());
-                newMeasurement.setMidRightThigh(midRightThigh.get());
-                newMeasurement.setUpperLeftThigh(upperLeftThigh.get());
-                newMeasurement.setUpperRightThigh(upperRightThigh.get());
-                newMeasurement.setAbdominalWaist(abdominalWaist.get());
-                newMeasurement.setLowerWaist(lowerWaist.get());
-                newMeasurement.setNarrowestWaist(narrowestWaist.get());
-                newMeasurement.setMeasurementDate(new Date());
-                newMeasurement.setUserId(user.getId());
-
-                measurementDao.insertMeasurements(newMeasurement);
-
-
+                measurementDao.insertMeasurements(measurement);
                 uiExecutor.execute(new Runnable() {
                     @Override
                     public void run() {
                         shouldShowProgressSpinner.set(false);
                         shouldShowSaveSuccessMessage.set(true);
-
-                        lowerLeftBicep.set(0.0f);
-                        lowerRightBicep.set(0.0f);
-                        rightBicep.set(0.0f);
-                        leftBicep.set(0.0f);
-                        leftCalf.set(0.0f);
-                        rightCalf.set(0.0f);
-                        chest.set(0.0f);
-                        leftForearm.set(0.0f);
-                        rightForearm.set(0.0f);
-                        highHip.set(0.0f);
-                        hip.set(0.0f);
-                        lowerRightThigh.set(0.0f);
-                        lowerLeftThigh.set(0.0f);
-                        midLeftThigh.set(0.0f);
-                        midRightThigh.set(0.0f);
-                        upperLeftThigh.set(0.0f);
-                        upperRightThigh.set(0.0f);
-                        abdominalWaist.set(0.0f);
-                        lowerWaist.set(0.0f);
-                        narrowestWaist.set(0.0f);
                     }
                 });
-
 
             }
         });
